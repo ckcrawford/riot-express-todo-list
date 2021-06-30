ad8c483580d46d94ce9242721fe262ff775f8011
Chad Crawford <ckcrawford81@gmail.com>
2021-06-29 20:45:54 -0500

    updated ip address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
     ssh
         .connect({
             // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-            host: '100.25.139.192',
+            host: '35.175.63.89',
             username: 'ubuntu',
             privateKey: 'retl-key.pem'
         })
